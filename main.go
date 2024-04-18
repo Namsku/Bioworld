b7ea757d541a0d1d81ef5f66788b2298722b7ba3
Namsku <namsku>
2024-04-18 23:16:15 +0200

    Updates

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-<<<<<<< HEAD
 
 	"github.com/namsku/bioworld/common"
 )
@@ -19,35 +18,19 @@ func main() {
 		Port:     1998,
 		Password: "",
 		Channels: []common.Channel{},
-=======
-)
-
-func main() {
-	// Initialize your server here
-	server := Server{
-		Port:     1998,
-		Password: "",
-		Channels: []Channel{},
->>>>>>> 7feef6c833cdcbd1427daa577833e32ab4cd4894
 	}
 	// ...
 
 	// Start a TCP server
-<<<<<<< HEAD
 	common.Debug.Println("Starting TCP Server...")
-=======
->>>>>>> 7feef6c833cdcbd1427daa577833e32ab4cd4894
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", server.Port))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer listener.Close()
 
-<<<<<<< HEAD
 	// Accept incoming connections
 	common.Debug.Println("Accepting Incoming Connections...")
-=======
->>>>>>> 7feef6c833cdcbd1427daa577833e32ab4cd4894
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -55,10 +38,6 @@ func main() {
 			continue
 		}
 
-<<<<<<< HEAD
 		go common.HandleConnection(conn, &server)
-=======
-		go handleConnection(conn, &server)
->>>>>>> 7feef6c833cdcbd1427daa577833e32ab4cd4894
 	}
 }
